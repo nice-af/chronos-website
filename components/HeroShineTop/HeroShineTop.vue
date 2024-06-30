a773c757c5fa738027d7d7fa33e4cc14cdc5ce59
Adrian Fahrbach <adrian@hatchery.io>
2024-06-30 22:10:46 +0200

    ✨ feat: add shine animation to hero

--- components/HeroShineTop/HeroShineTop.vue
+++ components/HeroShineTop/HeroShineTop.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="wrapper">
+    <div :class="[heroShineTop.shineContainer, 'is-shine-1']" ref="shineContainerRef">
+      <img
+        :class="heroShineTop.shine"
+        src="/assets/images/shine-1.png"
+        width="1114"
+        height="634"
+        alt="Lights shining from top" />
+      <img
+        :class="heroShineTop.shine"
+        src="/assets/images/shine-2.png"
+        width="1114"
+        height="634"
+        alt="Lights shining from top" />
+      <img
+        :class="heroShineTop.shine"
+        src="/assets/images/shine-3.png"
+        width="1114"
+        height="634"
+        alt="Lights shining from top" />
+      <img
+        :class="heroShineTop.shine"
+        src="/assets/images/shine-4.png"
+        width="1114"
+        height="634"
+        alt="Lights shining from top" />
+    </div>
     <div :class="heroShineTop.container">
       <p v-if="!!pill" :class="heroShineTop.pillContainer">
         <Button :label="pill.label" :href="pill.href" is-pill variant="secondary">
@@ -20,6 +46,8 @@ import type Button from '../Button/Button.vue';
 import { PhArrowRight } from '@phosphor-icons/vue';
 import { useResizeObserver } from '@vueuse/core';
 import { throttle } from 'lodash';
+
+const shineContainerRef = ref<HTMLElement | null>(null);
 
 const headlineRef = ref<HTMLElement | null>(null);
 const headlineLines = ref<number | null>(null);
@@ -44,6 +72,24 @@ const throttledUpdateLines = throttle(updateLines, 100);
 
 useResizeObserver(headlineRef, throttledUpdateLines);
 throttledUpdateLines();
+
+// Add shine classes
+let shineTimer: NodeJS.Timeout;
+onNuxtReady(() => {
+  let shineIndex = 2;
+  shineContainerRef.value.classList.remove(`is-shine-1`);
+  shineContainerRef.value.classList.add(`is-shine-${shineIndex}`);
+  shineTimer = setInterval(() => {
+    if (!shineContainerRef.value) return;
+    shineContainerRef.value.classList.remove(`is-shine-${shineIndex}`);
+    shineIndex = shineIndex === 4 ? 1 : shineIndex + 1;
+    shineContainerRef.value.classList.add(`is-shine-${shineIndex}`);
+  }, 2500);
+});
+
+onUnmounted(() => {
+  clearInterval(shineTimer);
+});
 </script>
 
 <style module="heroShineTop" lang="scss" src="./HeroShineTop.scss" />
