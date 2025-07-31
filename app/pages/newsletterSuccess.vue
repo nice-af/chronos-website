be7510ee1335f82544f551ddfc5d5290ab3e24b8
Adrian Fahrbach <adrianfahrbach@me.com>
2025-07-31 20:08:41 +0200

    feat: add newsletter success page

---
+++ app/pages/newsletterSuccess.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <div :class="newsletterSuccess.container">
+      <PhCheck size="64" weight="light" :class="newsletterSuccess.icon" />
+      <h1 :class="newsletterSuccess.headline">
+        {{ t('newsletterSuccess.headline') }}
+      </h1>
+      <p :class="[newsletterSuccess.description]">{{ t('newsletterSuccess.description') }}</p>
+      <NuxtLink to="/" :class="newsletterSuccess.link">{{ t('newsletterSuccess.linkText') }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NuxtLink } from '#components';
+import { defineI18nRoute, useI18n } from '#imports';
+import { PhCheck } from '@phosphor-icons/vue';
+
+const { t } = useI18n();
+
+defineI18nRoute({
+  paths: {
+    en: '/newsletter-success',
+    de: '/newsletter-success',
+  },
+});
+</script>
+
+<style lang="scss" src="../assets/styles/global.scss" />
+<style lang="scss" src="./newsletterSuccess.scss" module="newsletterSuccess" />
