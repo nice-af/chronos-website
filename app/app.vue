4c2807a300c7deac96c9dc37d343fef840d89fd0
Adrian Fahrbach <adrianfahrbach@me.com>
2025-07-31 20:12:17 +0200

    feat: add vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,5 @@
 <template>
+  <Analytics />
   <a href="#main" class="skip-to-content">{{ t('global.skipToContent') }}</a>
   <Header />
   <main id="main">
@@ -12,6 +13,7 @@ import Footer from '~/components/Footer/Footer.vue';
 import Header from '~/components/Header/Header.vue';
 import { NuxtPage } from '#components';
 import { useI18n } from '#imports';
+import { Analytics } from '@vercel/analytics/nuxt';
 
 const { t } = useI18n();
 </script>
