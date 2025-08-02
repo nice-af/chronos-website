e421cd50e153a0c99d9d3aaf3a33ef7397763da6
Adrian Fahrbach <adrianfahrbach@me.com>
2025-08-02 14:10:39 +0200

    feat: add more json ld schemas

--- app/app.vue
+++ app/app.vue
@@ -9,11 +9,12 @@
 </template>
 
 <script setup lang="ts">
-import Footer from '~/components/Footer/Footer.vue';
-import Header from '~/components/Header/Header.vue';
 import { NuxtPage } from '#components';
 import { useI18n, useRuntimeConfig, useSeoMeta } from '#imports';
+import { useJsonld } from '#jsonld';
 import { Analytics } from '@vercel/analytics/nuxt';
+import Footer from '~/components/Footer/Footer.vue';
+import Header from '~/components/Header/Header.vue';
 
 const { t } = useI18n();
 const config = useRuntimeConfig();
@@ -24,6 +25,37 @@ useSeoMeta({
   description: t('meta.description'),
   ogDescription: t('meta.description'),
   ogImage: `${config.public.BASE_URL}/assets/images/og-image.jpg`,
+});
+
+/**
+ * Add SoftwareApplication schema to the page
+ */
+useJsonld({
+  '@context': 'https://schema.org',
+  '@type': 'SoftwareApplication',
+  name: 'Chronos App',
+  description: t('meta.description'),
+  operatingSystem: 'macOS',
+  applicationCategory: 'ProductivityApplication',
+  image: `${config.public.BASE_URL}/assets/images/og-image.jpg`,
+  offers: {
+    '@type': 'Offer',
+    price: '3.99',
+    priceCurrency: 'EUR',
+    availability: 'https://schema.org/InStock',
+    url: config.public.DOWNLOAD_MACOS_URL,
+  },
+});
+
+/**
+ * Add Organization schema to the page
+ */
+useJsonld({
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  name: 'Nice A.F.',
+  url: 'https://github.com/nice-af',
+  logo: 'https://avatars.githubusercontent.com/u/88162758',
 });
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,4 +17,38 @@ import CtaSection from '~/components/CtaSection/CtaSection.vue';
 import FeatureCarousel from '~/components/FeatureCarousel/FeatureCarousel.vue';
 import Features from '~/components/Features/Features.vue';
 import HeroShineTop from '~/components/HeroShineTop/HeroShineTop.vue';
+import { useJsonld } from '#jsonld';
+import { useI18n, useRuntimeConfig } from '#imports';
+
+const { t } = useI18n();
+const config = useRuntimeConfig();
+
+/**
+ * Add WebPage schema to the page
+ */
+useJsonld({
+  '@context': 'https://schema.org',
+  '@type': 'WebPage',
+  name: t('meta.title'),
+  description: t('meta.description'),
+  url: config.public.BASE_URL,
+});
+
+/**
+ * Update BreadcrumbList schema for the home page
+ */
+useJsonld({
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  itemListElement: [
+    {
+      '@type': 'ListItem',
+      position: 1,
+      item: {
+        '@id': '/',
+        name: t('global.home'),
+      },
+    },
+  ],
+});
 </script>
