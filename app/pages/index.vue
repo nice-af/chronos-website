21df9faf9e7e7d64d253fa7394e78f4979ddc7bc
Adrian Fahrbach <adrianfahrbach@me.com>
2025-08-02 13:56:41 +0200

    feat: adjust hero shine animation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,7 +14,7 @@
 import AppSettingsProvider from '~/components/AppSettingsProvider/AppSettingsProvider.vue';
 import Configurator from '~/components/Configurator/Configurator.vue';
 import CtaSection from '~/components/CtaSection/CtaSection.vue';
-import Features from '~/components/Features/Features.vue';
 import FeatureCarousel from '~/components/FeatureCarousel/FeatureCarousel.vue';
+import Features from '~/components/Features/Features.vue';
 import HeroShineTop from '~/components/HeroShineTop/HeroShineTop.vue';
 </script>
